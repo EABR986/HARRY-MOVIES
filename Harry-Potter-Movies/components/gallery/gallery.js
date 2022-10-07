fb94acd6aee981bc2d781ec490fd05d67a166c03
mariasosaluna <maria.sosa.luna@gmail.com>
2022-10-07 08:44:51 +0200

    aplicacion arreglada. input funcional

--- Harry-Potter-Movies/components/gallery/gallery.js
+++ Harry-Potter-Movies/components/gallery/gallery.js
@@ -5,57 +5,38 @@ export const initFilms = () => {
     getFilms();
   }
   
-  // Declaramos una función asincrona -> necisitamos el async antes de los ()
-  const getFilms = async () => {
-    // Definimos el Try Catch para intentar recuperar los datos y sino capturar el error
-    try {
-      // Lanzar la petición -> await para guardarlos en la variable
-      const responsePromiseFilm = await fetch('http://localhost:3000/movies');
-      console.log()
-      // Transformar los datos a un formato legible -> Json => await para tranformar los datos y almacenarlos
-      const responseFilmToJson = await responsePromiseFilm.json();
-      console.log(responseFilmToJson)
-      // Invocamos a la funcíon que mappea los datos recibidos
-      tranformFilms(responseFilmToJson);
-    } catch (error) {
-      // Capturamos el error y lo sacamos por consola
-      console.log(error)
-    }
-  }
-  
-  // Mapeamos la info de la Api con los datos que nosotros necesitamos
-  const tranformFilms = (films) => {
-   console.log(films)
-    let elementsTransformed = films.map((film) => {
-      return {
-        id: film.id,
-        date: film.date,
-        title:film.title,
-        director: film.dierctor,
-        description: film.description,
-        poster: film.poster,
-        duration: film.duration,
-       
-      }
-    })}
-    // Llammos a la función de pintado -> template 
-     
-  
-  const inputTime= ()=>{
-    films.forEach((film) => {
-        const listFilm = `
-        <div class="flex relative">
-            <img alt="harry potter poster" class="poster"
-              src=${film.poster}>
-            <div>
-            <h1 class="title">${film.title}</h1>
-            <h2 class="director"> Direct by: ${film.director} || Total Duration: ${film.duration}</h2>
-            <p class="description">${film.description}</p>
+// Declaramos una función asincrona -> necisitamos el async antes de los ()
+export const getFilms = async () => {
+  // Definimos el Try Catch para intentar recuperar los datos y sino capturar el error
+  try {
+    // Lanzar la petición -> await para guardarlos en la variable
+    const responsePromiseFilm = await fetch('http://localhost:3000/movies');
+    // Transformar los datos a un formato legible -> Json => await para tranformar los datos y almacenarlos
+    const responseFilmToJson = await responsePromiseFilm.json();
 
-        </div>
-        `
-        app.innerHTML += listFilm; 
-})};
+    // Invocamos a la funcíon que mappea los datos recibidos
+    return responseFilmToJson;
+  } catch (error) {
+    // Capturamos el error y lo sacamos por consola
+    console.log(error)
+  }
+}
+  
+// Mapeamos la info de la Api con los datos que nosotros necesitamos
+const tranformFilms = (films) => {
+  let elementsTransformed = films.map((film) => {
+    return {
+      id: film.id,
+      date: film.date,
+      title:film.title,
+      director: film.dierctor,
+      description: film.description,
+      poster: film.poster,
+      duration: film.duration,
+      
+    }
+  })}
+  
     
 
   
--- Harry-Potter-Movies/components/input/input.js
+++ Harry-Potter-Movies/components/input/input.js
@@ -1,28 +1,33 @@
-console.log("pepe")
+import { getFilms } from "../gallery/gallery";
+
 const myDuration= document.querySelector("#myInput");
-myDuration.addEventListener('input', updateValue);
-
-function updateValue(e) {
+myDuration.addEventListener('input', (e) => {
     const duration= e.target.value;  
-    console.log(duration)
-}
+    inputTime(duration)
+});
 
 
-export const inputTime= (myDuration)=>{
-    const films= getFilms;
+export const inputTime= async (myDuration)=>{
+    const films= await getFilms();
+
+    app.innerHTML = "";
     films.forEach((film) => {
-        if(myDuration <= film.duration ){
-        const listFilm = `
-        <div class="flex relative">
-            <img alt="harry potter poster" class="poster"
-              src=${film.poster}>
-            <div>
-            <h1 class="title">${film.title}</h1>
-            <h2 class="director"> Direct by: ${film.director} || Total Duration: ${film.duration}</h2>
-            <p class="description">${film.description}</p>
+        
+    if(myDuration >= film.duration ) {
+        
+        const listFilm = ` 
+            <div class="flex relative">
+                <img alt="harry potter poster" class="poster"
+                src=${film.poster}>
+                <div>
+                <h1 class="title">${film.title}</h1>
+                <h2 class="director"> Direct by: ${film.director} || Total Duration: ${film.duration}</h2>
+                <p class="description">${film.description}</p>
 
-        </div>
+            </div>
         `
-         } app.innerHTML += listFilm; 
+        app.innerHTML += listFilm; 
+
+    } 
+         
 })} 
-updateValue(inputTime)
