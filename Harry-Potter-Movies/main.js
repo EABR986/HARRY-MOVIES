af5a0cb8abe6d1b219b847c4a49522b9499a8081
EABR986 <eduardoblanco198@gmail.comndgit config --global user.email eduardoblanco1987@gmail.com>
2022-10-10 08:39:31 +0200

    feat:server and styles

--- Harry-Potter-Movies/main.js
+++ Harry-Potter-Movies/main.js
@@ -2,4 +2,6 @@
 
 import './components/style/style.css';
 import './components/input/input';
-
+// import './components/buttons/select-house-button';
+// import { griffPicker } from './components/buttons/select-house-button';
+// griffPicker()
